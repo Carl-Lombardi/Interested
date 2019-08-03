8ac2298932c6f1afe4de2ed9d45d32af3fdc2185
sublimerocks86 <carl.h.lombardi@gmail.com>
2019-08-03 12:06:18 -0400

    Upload Two

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.png";
+import Toolbar from "./Components/Toolbar/Toolbar.js";
+import SideToggle from "./Components/SideToggle/SideToggle.js";
+import Backdrop from "./Components/Backdrop/Backdrop.js";
 import "./App.css";
+import "./Components/Toolbar/Toolbar.css";
 
 class App extends Component {
+  state ={
+    SideToggleOpen: false
+};
+
+  SideToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {SideToggleOpen: !prevState.SideToggleOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({SideToggleOpen: false});
+  };
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Interested!?</h1>
-        </div>
-          <img className="App-intro" src={logo} className="App-logo" alt="logo" />
+    let backdrop;
+
+    if (this.state.SideToggleOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+   return (
+      <div style= {{height: "100%"}}>
+        <Toolbar sideClickHandler={this.SideToggleClickHandler} />
+        <SideToggle show={this.state.SideToggleOpen} />
+        {backdrop}
+        <p>This is the page content!</p>
       </div>
     );
   }
