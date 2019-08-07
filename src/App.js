96eba709ef23e12152e64b1b4959125af484a88d
Carl Lombardi <carl.h.lombardi@gmail.com>
2019-08-06 21:09:28 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Toolbar from "./components/toolbar/toolbar.js";
+import SideToggle from "./components/sideToggle/sideToggle.js";
+import Backdrop from "./components/backdrop/backdrop.js";
+import "./App.css";
+import "./components/toolbar/toolbar.css";
+
+
+class App extends Component {
+  state ={
+    SideToggleOpen: false
+};
+
+  SideToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {SideToggleOpen: !prevState.SideToggleOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({SideToggleOpen: false});
+  };
+
+  render() {
+    let backdrop;
+
+    if (this.state.SideToggleOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+   return (
+      <div style= {{height: "100%"}}>
+        <Toolbar sideClickHandler={this.SideToggleClickHandler} />
+        <SideToggle show={this.state.SideToggleOpen} />
+        {backdrop}
+        <p>This is the page content!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
