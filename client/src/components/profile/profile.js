bc6383fec743976257024ffc9a17f8ffffe3d5cb
Chris Magesty <cmagesty@gmail.com>
2019-08-10 13:34:33 -0400

    firebasefeature

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,77 +1,127 @@
 import React from 'react';
-// import  ControlledOpenSelect from "../../components/form/form";
 
 class Profile extends React.Component {
-    prof = {
+    constructor(props) {
+    super(props);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeGender = this.onChangeGender.bind(this);
+    this.onChangeAge = this.onChangeAge.bind(this);
+    this.onChangeState = this.onChangeState.bind(this);
+    this.onChangeHobbies =  this.onChangeHobbies.bind(this);
+    this.onChangeEmail =  this.onChangeEmail.bind(this);
+
+    this.state = {
         Name: "",
         Gender: "",
         Age: "",
         State: "",
         Hobbies: "",
-        Email: "",
-    };
+        Email: ""
+    }
+}
 
-change = e => {
+onChangeName(e) {
     this.setState({
-        [e.target.name]: e.target.value
+      Name: e.target.value
     });
-};
-
-onSubmit = e => {
+  }
+onChangeGender(e) {
+    this.setState({
+      Gender: e.target.value
+    });
+  }
+onChangeAge(e) {
+    this.setState({
+      Age: e.target.value
+    });
+  }
+onChangeState(e) {
+    this.setState({
+      State: e.target.value
+    });
+  }
+onChangeHobbies(e) {
+    this.setState({
+      Hobbies: e.target.value
+    });
+  }
+onChangeEmail(e) {
+    this.setState({
+      Email: e.target.value
+    });
+  }
+  onSubmit(e) {
     e.preventDefault();
-    console.log(this.prof);
-};
+    this.setState({
+        Name: "",
+        Gender: "",
+        Age: "",
+        State: "",
+        Hobbies: "",
+        Email: ""
+    })
+  }
 
+  
 render() {
     return (
-        <form>
-            <input
-            name="Name"
-            placeholder="Name"
-            value= {this.prof.Name} 
-            onChange={e => this.change(e)}
-            />
-           <br />
-            <input
-            name="Gender"
-            placeholder="Gender"
-            value= {this.prof.Gender} 
-            onChange={e => this.change(e)}
-            />
-            <br />
-            <input
-            name="Age"
-            placeholder="Age"
-            value= {this.prof.Age} 
-            onChange={e => this.change(e)}
-            />
-            <br />
-            <input
-            name="State"
-            placeholder="State"
-            value= {this.prof.State} 
-            onChange={e => this.change(e)}
-            />
-            <br />
-            <input
-            name="Hobbies"
-            placeholder="Hobbies"
-            value= {this.prof.Hobbies} 
-            onChange={e => this.change(e)}
-            />
-            <br />
-            <input
-            name="Email"
-            placeholder="Email"
-            value= {this.prof.Email} 
-            onChange={e => this.change(e)}
-            />
-            <br />
-        <button onClick={e => this.onSubmit(e)}>Submit</button>
-    </form>
-    );
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Name:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.Name}
+                      onChange={this.onChangeName}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Gender:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.Gender}
+                      onChange={this.onChangeGender}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Age:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.Age}
+                      onChange={this.onChangeAge}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>State:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.State}
+                      onChange={this.onChangeState}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Hobbies:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.Hobbies}
+                      onChange={this.onChangeHobbies}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Email:  </label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      value={this.state.Email}
+                      onChange={this.onChangeEmail}
+                      />
+                </div>
+                    <button><input type="submit" value="Submit!" className="btn btn-primary"/></button>
+            </form>
+    )}
 }
-}
-// <ControlledOpenSelect></ControlledOpenSelect>
-
 export default Profile;
