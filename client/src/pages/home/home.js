3633b813145dbbfca2d3920bf331d02efa2399a4
cdyall <cdyall1@gmail.com>
2019-08-24 17:59:37 -0400

    Fixed-it

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import logo from "./images/logo.png";
 import "./home.css";
-import ContainedButtons from "../../components/button/button.js";
 import fire from '../../config/Fire';
-// import Nav from "../../components/Routes/routes"
-// import Toolbar from "../../components/toolbar/toolbar"
+import  Toolbar from "../../components/toolbar/toolbar"
+
+
 
 class Home extends Component {
     constructor(props) {
@@ -22,19 +22,15 @@ class Home extends Component {
             <div className="Home">
                 <div className="Home-header">
 
-                    <h2>INTERESTED!?</h2>
-                     
-                    {/* <Toolbar></Toolbar>  */}
-                    {/* <Nav></Nav>  */}
-                    <button onClick={this.logout}>Logout</button>
+                    <Toolbar></Toolbar>
+                   
                 </div>
 
 
                 <img src={logo} className="Home-logo" alt="logo" />
 
                 
-
-              <ContainedButtons></ContainedButtons>
+                <button onClick={this.logout}>Logout</button>
 
             </div>
 
