5b75abcc34eecc8ae709b9056999b0bbb09b286d
Chris Magesty <cmagesty@gmail.com>
2019-08-29 11:10:45 -0400

    home.js

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import logo from "./images/logo.png";
 import "./home.css";
-import fire from '../../config/Fire';
 import  Toolbar from "../../components/toolbar/toolbar"
 
 
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.logout = this.logout.bind(this);
-    }
-
-
-    logout() {
-        fire.auth().signOut();
-    }
+  
 
     render() {
         return (
@@ -26,12 +17,8 @@ class Home extends Component {
                    
                 </div>
 
-
                 <img src={logo} className="Home-logo" alt="logo" />
-
-                
-                <button onClick={this.logout}>Logout</button>
-
+            
             </div>
 
         );
