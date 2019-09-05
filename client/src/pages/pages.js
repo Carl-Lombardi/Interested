e6c9dbc33d5b8d3b59aeec8837b3cb19da7e9992
smagesty <46585827+smagesty@users.noreply.github.com>
2019-09-04 22:05:47 -0400

    Update pages.js

--- client/src/pages/pages.js
+++ client/src/pages/pages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PostForm from "../components/profile/Test";
 import Results from "../pages/results/results";
-import Home from "../pages/Home/home"
+import Home from "./Home/home";
 
 
 
